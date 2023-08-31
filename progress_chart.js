b3c857c9f204cee49e525a7b911d51ab57e70c20
brokenmental <commontoday@naver.com>
2023-08-31 18:10:27 +0900

    함수 호출 시 차트 객체를 삭제 후 재생성하는 방식으로 변경(애니메이션을 다시 표현하기 위해서)

--- progress_chart.js
+++ progress_chart.js
@@ -1,6 +1,17 @@
 const setChartPercent = (setNumber) => {
-    const ptChart = document.getElementById("ptChart");
-    const ptChartStr = document.getElementById("ptChartStr");
+    const ptChartArea = document.getElementById("ptChartArea");
+    let ptChart = document.getElementById("ptChart");
+    ptChartArea.removeChild(ptChart);
+
+    ptChart = document.createElement('div');
+    ptChart.className = 'progress-chart';
+    ptChart.id = 'ptChart';
+
+    const ptChartStr = document.createElement('span'); //document.getElementById("ptChartStr");
+    ptChartStr.className = 'progress-chart-str';
+    ptChartStr.id = 'ptChartStr';
+    ptChart.appendChild(ptChartStr);
+    ptChartArea.appendChild(ptChart);
 
     ptChart.style.maxWidth = setNumber + "%";
     ptChartStr.innerText = setNumber + "%";
