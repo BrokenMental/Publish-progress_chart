f974eee6d8ff9c5504928f6bc289b816bb6557f3
loooooooookaaa <commontoday@naver.com>
2023-09-01 21:46:02 +0900

    폭 크기에 따라 퍼센트가 안보이는 문제 수정

--- progress_chart.js
+++ progress_chart.js
@@ -3,18 +3,27 @@ const setChartPercent = (setNumber) => {
     let ptChart = document.getElementById("ptChart");
     ptChartArea.removeChild(ptChart);
 
-    ptChart = document.createElement('div');
-    ptChart.className = 'progress-chart';
-    ptChart.id = 'ptChart';
+    ptChart = document.createElement("div");
+    ptChart.className = "progress-chart";
+    ptChart.id = "ptChart";
 
-    const ptChartStr = document.createElement('span'); //document.getElementById("ptChartStr");
-    ptChartStr.className = 'progress-chart-str';
-    ptChartStr.id = 'ptChartStr';
+    const ptChartStr = document.createElement("span"); //document.getElementById("ptChartStr");
+    ptChartStr.className = "progress-chart-str";
+    ptChartStr.id = "ptChartStr";
     ptChart.appendChild(ptChartStr);
     ptChartArea.appendChild(ptChart);
 
     ptChart.style.maxWidth = setNumber + "%";
     ptChartStr.innerText = setNumber + "%";
-}
+
+    if (setNumber < 12 && setNumber >= 8) {
+        ptChartStr.style.fontSize = "small";
+    } else if (setNumber < 8) {
+        ptChartStr.style.fontSize = "large";
+        ptChartStr.style.left = "100%";
+        ptChartStr.style.transform = "translate(10%, -50%)";
+        ptChartStr.style.color = "#4B78A8";
+    }
+};
 
 setChartPercent(50);
