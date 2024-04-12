61e58c4c5d0b5893cbd09ef1333d128e51db495e
loooooooookaaa <commontoday@naver.com>
2024-04-12 19:32:11 +0900

    함수 변경 및 초기화 적용

--- progress_chart2/progress_chart2.js
+++ progress_chart2/progress_chart2.js
@@ -17,8 +17,8 @@ const setChartPercent = (setNumber) => {
     ptChartArea.appendChild(ptChart);
 
     progressChart2_targetCnt = setNumber;
-    requestAnimationFrame(increaseCnt);
-    //increaseCnt();
+    //requestAnimationFrame(increaseCnt); //화면 그대로 진행 시 초기화가 안됨
+    increaseCnt();
 
     ptChart.style.maxWidth = setNumber + "%";
     ptChartStr.innerText = setNumber + "%";
@@ -30,6 +30,7 @@ const setChartPercent = (setNumber) => {
     }
 };
 
+/*
 const increaseCnt = (timestamp) => {
     if(progressChart2_currentCnt < progressChart2_targetCnt) {
         const ptChart = document.getElementById("ptChart");
@@ -54,20 +55,36 @@ const increaseCnt = (timestamp) => {
         requestAnimationFrame(increaseCnt);
     }
 }
+*/
 
-/*
 const increaseCnt = () => {
+    const ptChart = document.getElementById("ptChart");
     const ptChartStr = document.getElementById("ptChartStr");
 
     const progressChart2Interval = setInterval(() => {
         if(progressChart2_currentCnt < progressChart2_targetCnt) {
             progressChart2_currentCnt++;
             ptChartStr.innerText = progressChart2_currentCnt + '%';
+
+            if(progressChart2_currentCnt < 30) {
+                ptChart.style.backgroundColor = '#FF0000';
+                ptChartStr.style.color = '#FF0000';
+            }else if(progressChart2_currentCnt > 25 && progressChart2_currentCnt <= 50 ) {
+                ptChart.style.backgroundColor = '#FFA500';
+                ptChartStr.style.color = '#FFA500';
+            }else if(progressChart2_currentCnt > 50 && progressChart2_currentCnt <= 75 ) {
+                ptChart.style.backgroundColor = '#008000';
+                ptChartStr.style.color = '#008000';
+            }else {
+                ptChart.style.backgroundColor = '#4B78A8';
+                ptChartStr.style.color = '#4B78A8';
+            }
         }else {
-            clearInterval(progressChart2Interval)
+            clearInterval(progressChart2Interval);
+            progressChart2_currentCnt = 0;
+            progressChart2_targetCnt = 0;
         }
     }, 20);
 }
-*/
 
 setChartPercent(100);
